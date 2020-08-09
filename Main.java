b1b6afd62851a62978b176168144b927649e67a1
Benrabah Mohamed <benrabahmohamed530@gmail.com>
2020-08-09 10:44:02 +0200

    update2
    convert decimal to octal

--- Main.java
+++ Main.java
@@ -1,15 +1,20 @@
 package numeralSystemConverter;
 
+import java.util.Scanner;
 
 public class Main {
+	public static final Scanner sc = new Scanner(System.in);
 	
 	public static void main(String[] args) {
-		int nbr = 2;
-		String nbrBin = convertDecToBin(nbr);
-		System.out.printf("%d is equal to 0b%s", nbr, nbrBin);
+		int nbr = sc.nextInt();
+		String nbrBin = convertDecToOctal(nbr);
+		System.out.printf("%c", nbrBin.charAt(nbrBin.length() - 1));
 	}
 	
 	static String convertDecToBin(int val) {
 		return Integer.toBinaryString(val);
+	}
+	static String convertDecToOctal(int val) {
+		return Integer.toOctalString(val);
 	}
 }
