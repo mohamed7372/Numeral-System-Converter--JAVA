2e529f5de50d8930ea7b85b32a605f94ab2f10d5
Benrabah Mohamed <benrabahmohamed530@gmail.com>
2020-09-17 14:15:34 +0200

    update4
    convert from dec to any number base and opposite. and you can convert from number to number

--- Main.java
+++ Main.java
@@ -6,39 +6,30 @@ public class Main {
 	public static final Scanner sc = new Scanner(System.in);
 	
 	public static void main(String[] args) {
-		int nbr = sc.nextInt();
-		int system = sc.nextInt();
+		int source = sc.nextInt();
+		String nbr = sc.next();
+		int traget = sc.nextInt();
 		
-		convertToSys(nbr, system);
-	}
-	
-	static String convertDecToBin(int val) {
-		return Integer.toBinaryString(val);
-	}
-	static String convertDecToOctal(int val) {
-		return Integer.toOctalString(val);
-	}
-	static String convertDecToHex(int val) {
-		return Integer.toHexString(val);
-	}
-
-	static void convertToSys(int val, int system) {
-		String cal;
-		switch (system) {
-		case 2:
-			cal = convertDecToBin(val);
-			System.out.printf("0b%s", cal);
-			break;
-		case 8: 
-			cal = convertDecToOctal(val);
-			System.out.printf("0%s", cal);
-			break;
-		case 16: 
-			cal = convertDecToHex(val);
-			System.out.printf("0x%s", cal);
-			break;
-		default:
-			System.out.println("error");
+		int number = source == 1 ? nbr.length() : Integer.parseInt(nbr, source);
+		if (traget == 10) {
+			if (source != 1)
+				System.out.println(number);
+			else {
+				int res = 0;
+				for (int i = 0; i < number; i++)
+					res++;
+				System.out.println(res);
+			}
 		}
+		else if (source == 10) {
+			if (traget == 1) 
+				for (int i = 0; i < Integer.valueOf(nbr); i++) 
+					System.out.print(1);
+			else
+				System.out.println(Integer.toString(Integer.valueOf(nbr), traget));
+		}
+		else 
+			System.out.println(Integer.toString(Integer.valueOf(number), traget));
+		
 	}
 }
